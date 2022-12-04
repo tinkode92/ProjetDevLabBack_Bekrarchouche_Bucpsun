d45c8a9d31bfa7c2375c651c524463dcf43fd4ba
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-12-04 13:48:38 +0100

    :building_construction: add index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TEST</title>
+</head>
+<body>
+
+<div class="grid grid-cols-2 grid-rows-3">
+    <div class="bg-red-500 row-span-4">
+        FORM
+    </div>
+    <div class="bg-blue-500">2</div>
+    <div class="bg-pink-500">3</div>
+    <div class="bg-purple-500">4</div>
+</div>
+
+
+</body>
+</html>
