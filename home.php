0028ee1bd73a683840972c9086a26a29af853967
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-12-04 16:21:13 +0100

    :sparkles: add logout functionnality

--- home.php
+++ home.php
@@ -7,14 +7,18 @@ require_once 'src/connection.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Accueil Dywiki's</title>
 </head>
 <body>
 
-<?php echo 'Hello ' .  $_SESSION['user_last_name'] . ' ' . $_SESSION['user_name'] . ' !'; ?>
+<div class="flex flex-col">
+    <?php echo 'Hello ' .  $_SESSION['user_last_name'] . ' ' . $_SESSION['user_name'] . ' !'; ?>
+    <?php echo '<a class="text-[#7BC8FF]" href="src/logout.php' . '">Déconnexion</a>'; ?>
+</div>
 
 </body>
 </html>
