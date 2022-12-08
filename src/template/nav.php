495164a34e1553172a53ef2e9e59e079fb0af1c6
tinkode92 <yanis.bekrarchouche@edu.devinci.fr>
2022-12-08 09:07:33 +0100

    💄 add landing page and updates style

--- src/template/nav.php
+++ src/template/nav.php
@@ -1,52 +1,52 @@
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
-    <div class="container flex flex-wrap items-center justify-between mx-auto">
+<nav class="border-gray-200 px-2 sm:px-4 py-2.5 drop-shadow-2xl">
+    <div class="container flex flex-wrap items-center justify-between mx-auto drop-shadow-2xl">
         <a href="home.php" class="flex items-center">
-            <img src="src/assets/img/LOGO.png" class="h-12 mr-3 sm:h-9" alt="Dywiki's Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Dywiki's</span>
+            <img src="src/assets/img/LOGO.png" class="w-[118px] sm:w-[118px] px-4 rounded-full shadow-sm" alt="Dywiki's Logo" />
         </a>
         <div class="flex items-center md:order-2">
-            <button type="button" class="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 dark:bg-white" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+            <button type="button" class="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-900 focus:ring-gray-600 bg-white" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                 <span class="sr-only">Open user menu</span>
                 <img class="w-8 h-8 rounded-full" src="src/assets/img/usser.png" alt="user photo">
             </button>
             <!-- Dropdown menu -->
-            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 absolute top-0 mt-16 right-0 max-md:mr-4 2xl:mr-6" id="user-dropdown">
+            <div class="z-50 hidden my-4 text-base list-none  divide-y divide-gray-100 rounded shadow divide-gray-600 absolute top-0 mt-16 right-0 max-md:mr-4 2xl:mr-6" id="user-dropdown">
                 <div class="px-4 py-3">
-                    <span class="block text-sm text-gray-900 dark:text-white"><?php echo 'Bonjour ' .  $_SESSION['user_last_name'] . ' ' . $_SESSION['user_name'] . ' !'?></span>
-                    <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400"><?php echo $_SESSION["user_email"]?></span>
+                    <span class="block text-sm text-gray-900 text-black"><?php echo 'Bonjour ' .  $_SESSION['user_last_name'] . ' ' . $_SESSION['user_name'] . ' !'?></span>
+                    <span class="block text-sm font-medium text-gray-500 truncate text-gray-400"><?php echo $_SESSION["user_email"]?></span>
                 </div>
                 <ul class="py-1" aria-labelledby="user-menu-button">
                     <li>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Vos albums</a>
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 text-gray-600 hover:text-black">Vos albums</a>
                     </li>
                     <li>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Paramètres</a>
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 text-gray-600 hover:text-black">Paramètres</a>
                     </li>
                     <li>
-                        <a href="src/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Déconnexion</a>
+                        <a href="src/logout.php" class="block px-4 py-2 text-sm text-sm text-gray-700 text-gray-600 hover:text-black">Déconnexion</a>
                     </li>
                 </ul>
             </div>
-            <button data-collapse-toggle="mobile-menu-2" id="btn-drop" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+            <button data-collapse-toggle="mobile-menu-2" id="btn-drop" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 mt-4 border border-yellow-200 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray rounded md:bg-transparent md:text-yellow-500 md:p-0 text-gray" aria-current="page">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded md:hover:bg-transparent md:hover:text-gray-700 md:p-0 text-gray-400 md:hover:text-black  hover:text-black md:hover:bg-transparent border-gray-700">About</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded md:hover:bg-transparent md:hover:text-gray-700 md:p-0 text-gray-400 md:hover:text-black hover:text-black md:hover:bg-transparent border-gray-700">Contact</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded md:hover:bg-transparent md:hover:text-gray-700 md:p-0 text-gray-400 md:hover:text-black  hover:text-black md:hover:bg-transparent border-gray-700">Blog</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
