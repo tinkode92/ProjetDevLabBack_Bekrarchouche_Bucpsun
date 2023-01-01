82c71d55cfb0a4102fc922732262e00ec2e0fda1
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2023-01-01 17:37:42 +0100

    :bug: Fix search url

--- search.php
+++ search.php
@@ -20,7 +20,7 @@ require_once 'src/connection.php';
 
     <?php require_once "src/template/nav.php"?>
 
-    <form class="search relative mt-10 mx-auto border-2 border-blue-700 bg-blue-500 rounded-full w-[70vw] h-[5vw]" method="get" action="/search.php">
+    <form class="search relative mt-10 mx-auto border-2 border-blue-700 bg-blue-500 rounded-full w-[70vw] h-[5vw]" method="get" action="search.php">
         <input type="text" name="query" id="search-input" class="text-[3vw] text-white outline-none bg-transparent w-[55vw] h-full absolute top-0 left-10">
         <button type="submit" class="w-[10vw] h-full bg-blue-500 float-right border-0 hover:bg-blue-700 duration-300 p-1 px-5 cursor-pointer rounded-full">
             <svg class="w-full h-full fill-white" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"/></svg>
--- src/template/nav.php
+++ src/template/nav.php
@@ -20,7 +20,7 @@
                 </li>
             </ul>
         </div>
-        <form method="get" action="/search.php" class="search relative ml-auto mr-10 border-2 border-gray-400 rounded-full w-[180px] h-8">
+        <form method="get" action="./search.php" class="search relative ml-auto mr-10 border-2 border-gray-400 rounded-full w-[180px] h-8">
             <input type="text" name="query" class="text-base text-gray-400 outline-none bg-transparent w-[100px] h-full absolute top-0 left-4">
             <button type="submit" class="w-[50px] h-full cursor-pointer rounded-full shadow-transparent float-right hover:bg-gray-400 duration-300 p-1">
                 <svg class="w-full h-full hover:fill-gray-900 fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"/></svg>
