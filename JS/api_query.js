cde5213ea1098910e4ebb2e255f10458f1d811ca
Denis Bucspun <bucspun.d@gmail.com>
2023-01-02 12:19:19 +0100

    :sparkles: Switch language to French language

--- JS/api_query.js
+++ JS/api_query.js
@@ -1,16 +1,16 @@
 const api_key = "e63fb2ad752c2e17625b63265a27a32a"
 
 function getGenres() {
-    return fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + api_key)
+    return fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + api_key + '&language=fr-FR')
         .then(response => response.json());
 }
 
 function getMoviesForGenre(genre, pagination) {
-    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=${genre}&page=${pagination}`)
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=${genre}&page=${pagination}` + '&language=fr-FR')
         .then(response => response.json())
 }
 
 function getMoviesForSearch(search, pagination) {
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}&page=${pagination}`)
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}&page=${pagination}` + '&language=fr-FR')
         .then(response => response.json())
 }
