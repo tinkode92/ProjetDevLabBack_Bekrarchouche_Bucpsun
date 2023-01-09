20d103d9a736523761982a3a6b962905a2e427e9
Denis Bucspun <bucspun.d@gmail.com>
2023-01-10 00:29:56 +0100

    :lipstick: Add style on invite.php

--- invite.php
+++ invite.php
@@ -22,12 +22,12 @@ require_once 'src/connection.php';
 
     <div class="flex fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
         <form class="flex flex-col p-4 place-content-center" method="post">
-            <h3>Inviter une personne à rejoindre l'album "<?= $_GET["name"] ?>"</h3>
+            <p>Inviter une personne à rejoindre l'album "<?= $_GET["name"] ?>"</p>
             <input type="email" class="p-1.5 border-gray-300 rounded-full bg-[#F3F3F3] my-2 drop-shadow-md" name="email" placeholder="Mail de la personne à inviter">
             <button type="submit" class="bg-[#F3F3F3] drop-shadow-md rounded-full p-1 mt-2">Inviter</button>
         </form>
     </div>
-
+<div class="flex flex-col justify-center">
     <?php
     $connection = new Connection();
 
@@ -35,14 +35,16 @@ require_once 'src/connection.php';
         $invite = $connection->invite($_GET["album_id"], $_POST["email"]);
 
         if (is_null($invite)) {
-            echo "Cet utilisateur n'existe pas";
+            echo "<p class='flex justify-center mt-4'>Cet utilisateur n'existe pas</p>";
         } else {
-            echo "Invitation bien envoyé !";
+            echo "<p class='flex justify-center mt-4'>Invitation bien envoyé !</p>";
             // TODO: notification center
-            echo '<br>' . $invite;
+            echo '<br>'. "<p class='flex justify-center'>". $invite . "</p>" . "<p class='flex justify-center mt-2'>Envoie ce lien à ton ami !</p>";
         }
     }
     ?>
+</div>
+
 <script src="JS/script.js"></script>
 </body>
 </html>
